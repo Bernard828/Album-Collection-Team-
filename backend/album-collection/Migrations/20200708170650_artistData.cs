4b2b86cca3445ec424509bf4a0f0f075bf7f42da
Tom <tomrambo95@yahoo.com>
2020-07-08 13:24:16 -0400

    added IRepository and ArtistRepository

---
+++ backend/album-collection/Migrations/20200708170650_artistData.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace album_collection.Migrations
+{
+    public partial class artistData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Artists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    ImageName = table.Column<string>(nullable: true),
+                    Age = table.Column<int>(nullable: false),
+                    HomeTown = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Artists", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Artists",
+                columns: new[] { "Id", "Age", "HomeTown", "ImageName", "Name" },
+                values: new object[] { 1, 55, "Alexandria", "image", "The Doors" });
+
+            migrationBuilder.InsertData(
+                table: "Artists",
+                columns: new[] { "Id", "Age", "HomeTown", "ImageName", "Name" },
+                values: new object[] { 2, 58, "New Jersey", "image", "Bon Jovi" });
+
+            migrationBuilder.InsertData(
+                table: "Artists",
+                columns: new[] { "Id", "Age", "HomeTown", "ImageName", "Name" },
+                values: new object[] { 3, 39, "Coral Gables", "image", "Sonic Youth" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Artists");
+        }
+    }
+}
