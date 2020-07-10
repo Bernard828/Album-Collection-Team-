6a7f6bb02e4ee93f983f63bd932735679bffc18f
roncwynn <rcwynn@gmail.com>
2020-07-10 11:22:29 -0400

    WIP landing page without button

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,23 +1,52 @@
+//import Header from "./components/header";
+import Footer from "./components/footer";
 import Artists from "./components/Artists";
-console.log("in main.js");
-console.log(Artists);
-console.log("after log artists");
-navArtist();
 
 const appDiv = document.querySelector('.app');
 
-function navArtist() {
-    const artistButton = document.querySelector('.nav__artist');
-    artistButton.addEventListener('click', function(){
-        console.log("word button clicked");
-        fetch("https://localhost:44313/api/artist")
-        .then(response => response.json())
-        .then(artists => {
-            appDiv.innerHTML = Artists(artists);
-            console.log(artists);
+pageBuild();
 
-        })
-        .catch(err => console.log(err))
-        // appDiv.innerHTML = Artist(artist);
+function pageBuild(){
+    //header();
+    footer();
+    navArtist2();
+}
+
+function header(){
+    const headerElement = document.querySelector('.header-container');
+    headerElement.innerHTML = Header();
+}
+
+function footer(){
+    const footerElement = document.querySelector('.footer-container');
+    footerElement.innerHTML = Footer();
+}
+
+// function navArtist() {
+//     const artistButton = document.querySelector('.nav__artist');
+//     artistButton.addEventListener('click', function(){
+//         console.log("word button clicked");
+//         fetch("https://localhost:44313/api/artist")
+//         .then(response => response.json())
+//         .then(artists => {
+//             appDiv.innerHTML = Artists(artists);
+//             console.log(artists);
+
+//         })
+//         .catch(err => console.log(err))
+//         // appDiv.innerHTML = Artist(artist);
+//     })
+// }
+
+function navArtist2() {
+    console.log("in navArtis2")
+    fetch("https://localhost:44313/api/artist")
+    .then(response => response.json())
+    .then(artists => {
+        appDiv.innerHTML = Artists(artists);
+        console.log(artists);
+
     })
+    .catch(err => console.log(err))
+    console.log(artists);
 }
