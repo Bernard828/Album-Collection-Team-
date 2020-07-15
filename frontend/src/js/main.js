aae0872b06f320fa72e30fffda48d5be2977a1cb
Bernard828 <bernard.lackey1@gmail.com>
2020-07-15 14:46:43 -0400

    WIP apiActions

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -3,11 +3,14 @@ import Footer from "./components/footer";
 import Artists from "./components/Artists";
 import Artist from "./components/Artist";
 import Albums from "./components/Albums";
+import apiActions from "./api/apiActions";
+import artistPost_section from "./components/artistPost_section";
+import ArtistPostSection from "./components/artistPost_section";
 
 const appDiv = document.querySelector('.app');
 
 
-export default function pageBuild(){
+export default function pageBuild() {
     header();
     footer();
     navHome();
@@ -15,20 +18,20 @@ export default function pageBuild(){
     //showAlbums();
 }
 
-function header(){
+function header() {
     const headerElement = document.querySelector('.header-container');
     headerElement.innerHTML = Header();
-    
+
 }
 
-function footer(){
+function footer() {
     const footerElement = document.querySelector('.footer-container');
     footerElement.innerHTML = Footer();
 }
 
-function navHome(){
+function navHome() {
     const homeButton = document.querySelector('.nav__home');
-    homeButton.addEventListener('click', function(){
+    homeButton.addEventListener('click', function () {
         appDiv.innerHTML = showArtists();
     })
 
@@ -36,42 +39,73 @@ function navHome(){
 
 function showArtists() {
     fetch("https://localhost:44313/api/artist")
-    .then(response => response.json())
-    .then(artists => {
-        appDiv.innerHTML = Artists(artists);
-        console.log(artists);
-        artistNameButton();
+        .then(response => response.json())
+        .then(artists => {
+            appDiv.innerHTML = Artists(artists);
+            console.log(artists);
+            artistNameButton();
 
-    })
-    .catch(err => console.log(err))
+        })
+        .catch(err => console.log(err))
 }
-function artistNameButton(){
+function artistNameButton() {
     const artistNameElement = document.querySelectorAll('.artist__name');
     //console.log(artistNameButton);
-    artistNameElement.forEach (element => {
-        element.addEventListener('click' , function(){
+    artistNameElement.forEach(element => {
+        element.addEventListener('click', function () {
             const artistId = element.id;
             console.log("artist Name clicked");
             console.log(artistId);
             fetch(`https://localhost:44313/api/artist/${artistId}`)
-            .then(response => response.json())
-            .then(artist => {
-                appDiv.innerHTML = Artist(artist);
-                showAlbums();
-            })
-            .catch(err => console.log(err))
+                .then(response => response.json())
+                .then(artist => {
+                    appDiv.innerHTML = Artist(artist);
+                    showAlbums();
+                })
+                .catch(err => console.log(err))
         })
     })
 }
-function showAlbums(){
-        const albumsButton = document.querySelector('.album__name');
-        albumsButton.addEventListener('click', function(){
-            fetch("https://localhost44313/api/album")
+function showAlbums() {
+    const albumsButton = document.querySelector('.album__name');
+    albumsButton.addEventListener('click', function () {
+        fetch("https://localhost44313/api/album")
             .then(response => response.json())
             .then(albums => {
                 appDiv.innerHTML = Albums(albums);
 
-        })
-        .catch(err => console.log(err))
+            })
+            .catch(err => console.log(err))
     })
+}
+appDiv.addEventListener('click', function () {
+    const addArtistSection = document.querySelector(".add-artist");
+    if (event.target.classList.contains("add-artist__button")) {
+        apiActions.getRequest(`https://localhost44313/api/artist`,
+            artist => {
+                console.log(artist);
+                addArtistSection.innerHTML = ArtistPostSection(artist);
+
+            })
+
+    }
+})
+appDiv.addEventListener('click'), function () {
+    if (event.target.classList.contains('add-artist__submit')) {
+        const artistName = event.target.ParentElement.querySelector(".add-artist__ArtistName").value;
+        const artistImageName = event.target.ParentElement.querySelector(".add-artist__ArtistImageName").value;
+        const artistAge = event.target.ParentElement.querySelector(".add-artist__ArtistAge").value;
+        const artistHomeTown = event.target.ParentElement.querySelector(".add-artist__ArtistHomeTown").value;
+        console.log(artistName);
+        var requestBody = {
+            Name: artistName, ImageName: artistImageName, Age: artistAge, HomeTown: artistHomeTown
+        }
+        apiActions.postRequest(
+            "https://localhost44313/api/artist", requestBody, newArtists => {
+                console.log("artist return from backend");
+                console.log(newArtists);
+                appDiv.innerHTML = Artist(newArtists);
+            }
+        )
+    }
 }
