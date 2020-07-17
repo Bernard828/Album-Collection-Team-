6ce1a740a4a3f865aafdba0bae20cea5a6eb6282
Bernard828 <bernard.lackey1@gmail.com>
2020-07-17 13:58:35 -0400

    modified navElement to remove second button

--- frontend/src/js/components/header.js
+++ frontend/src/js/components/header.js
@@ -1,7 +1,7 @@
 export default function Header() {
     return `<nav class='nav-container'>
             <a href="#" class='nav__home'</a>Home
-            <a href="#" class='nav__variable'</a>Variable
+            <a href="#" hidden="true" class='nav__variable'</a>Variable
          
     </nav>
     `
