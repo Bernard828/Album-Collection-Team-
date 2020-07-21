7b71808035b83d79a15260090cc139356127ecf4
Bernard828 <bernard.lackey1@gmail.com>
2020-07-21 10:54:51 -0400

    WIP song-list styling, started on album and artist page, add background image as well

--- frontend/src/js/components/album.js
+++ frontend/src/js/components/album.js
@@ -1,13 +1,13 @@
 export default function Album(album) {
     return `
   <h1>${album.name}</h1>
-    <ul>
+    <ol class="song-collection">
     ${album.songs.map(song => {
         return `
-        <li>${song.title}</li>
+        <li>${song.title} -- ${song.duration}</li>
         `
     }).join("")}
-    </ul>    
+    </ol>    
     <section class= "add-song">
     <button class= "add-song__button" Id="${album.id}">Add Song </button>
     <button class="delete-album__button" Id="${album.id}">Delete This Album</button>
